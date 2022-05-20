47f80b4f626cd091c88b2373ffb4d72f6083b5af
Zhewen <zhewen.pan@wisc.edu>
2022-05-20 01:22:51 -0500

    misc: adding fs run scripts

--- my_scripts/fs/fs.sh
+++ my_scripts/fs/fs.sh
@@ -1,25 +1,55 @@
 #!/bin/bash
+if [ $# -lt 1 ] 
+    then 
+        echo using default
+        sim='simsmall'
+        benchmark='dedup'
+        mesh=4
+        baseline=1
+    else
+        if [ $# -eq 2 ] 
+            then
+                sim='simsmall'
+                benchmark=$1
+                mesh=8
+                baseline=$2
+            else
+                sim=$1
+                benchmark=$2
+                mesh=$3
+                baseline=$4
+        fi
+fi
+
+let core=mesh*mesh
 
 l1_size='32kB'
 l1_assoc=4
-l2_size='128kB'
-l2_assoc=8
 
-# -d $out_dir/${benchmark}_${num_cores}c_${inp}_w${work_items}_${cfg} \
+if [ $baseline -eq 1 ] 
+    then 
+        l2_size='128kB'
+        l2_assoc=8
+    else
+        l2_size='168kB'
+        l2_assoc=8
+fi
 
+python3 my_scripts/fs/gen_script.py -s ${sim} -c ${core} -b ${benchmark}
+echo "run script generated"
 
 time build/X86/gem5.opt \
--d my_STATS/dedup_16_simsmall \
+-d my_STATS/${benchmark}_${core}_${sim} \
 configs/example/fs.py \
 --kernel ~/.cache/gem5/x86-linux-kernel-4.19.83 \
 --disk-image ~/.cache/gem5/x86-parsec \
---script /home/zhewen/repo/gem5-resources/src/parsec/gem5/my_scripts/fs/run.rcS \
+--script /home/zhewen/repo/gem5-resources/src/parsec/gem5/my_scripts/fs/${benchmark}_${sim}_${core}.rcS \
 --ruby \
 --restore-with-cpu TimingSimpleCPU \
---num-cpus=16 \
+--num-cpus=${core} \
 --num-dirs=4 \
 --mem-size=1GB \
---num-l2caches=16 \
+--num-l2caches=${core} \
 --l1d_size=${l1_size} \
 --l1i_size=${l1_size} \
 --l1d_assoc=${l1_assoc} \
@@ -28,6 +58,6 @@ configs/example/fs.py \
 --l2_assoc=${l2_assoc} \
 --network=garnet \
 --topology=MeshDirCorners_XY \
---mesh-rows=4 \
+--mesh-rows=${mesh} \
 --vcs-per-vnet=8 \
 --router-latency=1
